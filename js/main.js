b008802f759bd4bdd8cb6b85e49e164c72387662
Dmither <mr.d.graumann@gmail.com>
2023-07-14 11:01:00 +0300

    header update

--- js/main.js
+++ js/main.js
@@ -7,18 +7,3 @@ burger.addEventListener("click", function (event) {
   document.body.classList.toggle("freeze")
 })
 
-let links = Array.from(document.querySelectorAll(".header__link"));
-links.forEach(link => {
-  link.addEventListener("focus", function(event) {
-    burger.classList.add("active");
-    menu.classList.add("active");
-    document.body.classList.add("freeze")
-  })
-});
-links.forEach(link => {
-  link.addEventListener("blur", function(event) {
-    burger.classList.remove("active");
-    menu.classList.remove("active");
-    document.body.classList.remove("freeze")
-  })
-})
