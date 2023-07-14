786d81cde153b142a05247dda53aa9a59bed4783
Dmither <mr.d.graumann@gmail.com>
2023-07-14 15:49:13 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let burger = document.querySelector(".header__burger");
 let menu = document.querySelector(".header__menu");
+let wrapper = document.querySelector(".wrapper");
 
 burger.addEventListener("click", function (event) {
   burger.classList.toggle("active");
